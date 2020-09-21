484d214207d2567d7d460eb63fd82da547b121b4
hj <dv.zinke@gmail.com>
2020-09-21 23:25:22 +0900

    feat : 글 포스트 위젯 완성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/config/palette.dart';
 import 'package:flutter_facebook_responsive_ui/data/data.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/widgets.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -35,18 +36,31 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
         SliverToBoxAdapter(
-            child: CreatePostContainer(currentUser: currentUser)
-        ),
+            child: CreatePostContainer(currentUser: currentUser)),
         SliverPadding(
           padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
           sliver: SliverToBoxAdapter(
             child: Rooms(onlineUsers: onlineUsers),
           ),
         ),
-        SliverPadding(padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-        sliver: SliverToBoxAdapter(
-          child: Stories(currentUser: currentUser, stories: stories,),
-        ),)
+        SliverPadding(
+          padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: Stories(
+              currentUser: currentUser,
+              stories: stories,
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final Post post = posts[index];
+              return PostContainer(post: post);
+            },
+            childCount: posts.length,
+          ),
+        )
       ],
     ));
   }
---
+++ lib/widgets/post_container.dart
@@ -0,0 +1,220 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_responsive_ui/config/palette.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class PostContainer extends StatelessWidget {
+  final Post post;
+
+  const PostContainer({
+    Key key,
+    @required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      padding: EdgeInsets.symmetric(vertical: 8),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _PostHeader(post: post),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(post.caption),
+                post.imageUrl != null
+                    ? SizedBox.shrink()
+                    : SizedBox(
+                        height: 6,
+                      ),
+              ],
+            ),
+          ),
+          post.imageUrl != null
+              ? Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: CachedNetworkImage(imageUrl: post.imageUrl),
+                )
+              : SizedBox.shrink(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            child: _PostStats(post: post),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  final Post post;
+
+  const _PostHeader({
+    Key key,
+    @required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ProfileAvatar(imageUrl: post.user.imageUrl),
+        SizedBox(
+          width: 8,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.user.name,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '${post.timeAgo} · ',
+                    style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                  ),
+                  Icon(
+                    Icons.public,
+                    color: Colors.grey[600],
+                    size: 12,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        IconButton(icon: Icon(Icons.more_horiz), onPressed: () => print('More'))
+      ],
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  final Post post;
+
+  const _PostStats({
+    Key key,
+    @required this.post,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                  color: Palette.facebookBlue, shape: BoxShape.circle),
+              child: Icon(
+                Icons.thumb_up,
+                size: 10,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            Expanded(
+                child: Text(
+              '${post.likes}',
+              style: TextStyle(color: Colors.grey[600]),
+            )),
+            Text(
+              '댓글 ${post.comments}개',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            SizedBox(
+              width: 8,
+            ),
+            Text(
+              '공유 ${post.shares}회',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+          ],
+        ),
+        Divider(),
+        Row(
+          children: [
+            _PostButton(
+              icon: Icon(
+                MdiIcons.thumbUpOutline,
+                color: Colors.grey[600],
+                size: 20,
+              ),
+              label: '좋아요',
+              onTap: () => print('좋아요'),
+            ),
+            _PostButton(
+              icon: Icon(
+                MdiIcons.commentOutline,
+                color: Colors.grey[600],
+                size: 20,
+              ),
+              label: '댓글 달기',
+              onTap: () => print('댓글 달기'),
+            ),
+            _PostButton(
+              icon: Icon(
+                MdiIcons.shareOutline,
+                color: Colors.grey[600],
+                size: 25,
+              ),
+              label: '공유하기',
+              onTap: () => print('공유하기'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _PostButton extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final Function onTap;
+
+  const _PostButton({
+    Key key,
+    @required this.icon,
+    @required this.label,
+    @required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 12),
+            height: 25,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icon,
+                SizedBox(width: 4,),
+                Text(label),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
